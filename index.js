04353f6c75443ac35b1a51aabdc1aa31fcdb9366
mlky1234 <edb1631@naver.com>
2020-08-30 02:51:17 +0900

    기능 구현 디자인 개선

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const sign_in_button = document.getElementById('js_sign_in');
 const sign_up_button = document.getElementById('js_sign_up');
 const login_box = document.getElementById('login_form');
+const submit = document.getElementById('submit');
 
 login_box.style.display = "none";
 let flag = 1;
@@ -14,8 +16,18 @@ function handleLoginClick(){
     }
 }
 
+function handleSignUpClick(){
+    window.open("main.html","_self");
+}
+
+function handleSubmit(){
+
+}
+
 function init(){
-    sign_up_button.addEventListener("click",handleLoginClick);
+    sign_in_button.addEventListener("click",handleLoginClick);
+    sign_up_button.addEventListener("click",handleSignUpClick);
+    submit.addEventListener('submit',handleSubmit);
 }
 
 init();
---
+++ main.js
@@ -0,0 +1,14 @@
+const inputs = document.getElementsByClassName('form_input');
+
+
+function init(){
+    for(let i = 0; i < inputs.length;i++){
+        inputs[i].addEventListener('focus',(event) => {
+            event.target.style.boxShadow = "0px 1px 7px -1px rgba(183,222,75,1)";
+        });
+        inputs[i].addEventListener('blur',(event) => {
+            event.target.style.boxShadow = "0px 1px 7px -1px rgba(68, 71, 90, 0.58)";
+        });
+    }
+}
+init();
